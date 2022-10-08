f64e76868a351477d02f262913458b237c49655f
Kamil Marczak <kamil.codemate@gmail.com>
2022-10-08 19:45:07 +0200

    Created stats counting animation

---
+++ scripts/counter.js
@@ -0,0 +1,18 @@
+// const targetValues = [314000, 10921, 311, 94.3];
+const speed = 100; // less = faster
+// const incrementAmounts = [3140, 109.5, 3.12, 0.95];
+const incrementInterval = 20; // increment interval in miliseconds
+const numContainers = document.querySelectorAll('.statAmount');
+
+const targetValues = [...numContainers].map((element) => parseFloat(element.getAttribute('targetValue')));
+
+const incrementAmounts = targetValues.map((element) => element / speed);
+console.log(incrementAmounts);
+numContainers.forEach((element, index) => {
+  element.innerHTML = 0;
+  setInterval(() => {
+    if (element.innerHTML < targetValues[index]) {
+      element.innerHTML = Math.round(+element.innerHTML + incrementAmounts[index]);
+    }
+  }, incrementInterval);
+});
